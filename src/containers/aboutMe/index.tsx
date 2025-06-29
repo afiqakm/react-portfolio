00a3424c9055dcff94e5422099aeef8b92237128
Afiq Akmal <afiq98akmal@gmail.com>
2025-06-30 03:18:33 +0800

    chore: remove unused import

--- src/containers/aboutMe/index.tsx
+++ src/containers/aboutMe/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { useNavActions } from '~utils/navActions';
 
 const AboutMe = () => {
--- src/navigation/PrivateRoutes.tsx
+++ src/navigation/PrivateRoutes.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const PrivateRoutes: FC = () => {
     // const isAuthenticated = Auth.getAuthToken();
